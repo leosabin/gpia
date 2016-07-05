b0d87d21e085f627fe240473b8bb9dd3bab8aac4
Leo Sabin <leo@Leos-MacBook-Pro.local>
2016-07-05 19:29:47 -0400

    did this work?

---
+++ src/main/java/com/example/leo/myapplication/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.leo.myapplication;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Button;
+
+import java.text.DecimalFormat;
+
+
+public class MainActivity extends ActionBarActivity {
+
+}
